b8167501d6e07de57f7f2b5b6647c48f8b473332
rajkamal-ctrl <raj2004kalita@gmail.com>
2025-07-17 10:39:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+$('.testimonials-container').owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    margin:10,
+    nav:true,
+    navText:["<i class='fa-solid fa-arrow-left'></i>",
+             "<i class='fa-solid fa-arrow-right'></i>"],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+    }
+})
+
+        var btn = document.getElementById("btn-1");
+        var tablinks = document.getElementsByClassName("tab-links");
+        var tabcontents = document.getElementsByClassName("tab-contents");
+        
+        function opentab(tabname){
+            for(tablink of tablinks){
+                tablink.classList.remove("active-link");
+            }
+            for(tabcontent of tabcontents){
+                tabcontent.classList.remove("active-tab");
+            }
+            event.currentTarget.classList.add("active-link");
+            document.getElementById(tabname).classList.add("active-tab")
+        }
+        btn.onclick = function(){
+            document.body.classList.toggle("dark-theme");
+            if(document.body.classList.contains("dark-theme")){
+                btn.src = "image/sun.png" ;
+            }else{
+                btn.src = "image/moon.png";
+            }
+        }
+
+
+let calcScrollValue = () =>{
+    let scrollProgress = document.getElementById("progress");
+    let progressValue = document.getElementById("progress-value");
+    let pos = document.documentElement.scrollTop;
+    let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100) / calcHeight);
+    if (pos > 100){
+        scrollProgress.style.display = "grid";
+    } else{
+        scrollProgress.style.display = "none";
+    }
+    scrollProgress.addEventListener("click", () => {
+        document.documentElement.scrollTop = 0;
+    });
+    
+    scrollProgress.style.background = `conic-gradient ( #ff4321 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+
+};
+
+window.onscroll= calcScrollValue;
+window.onload= calcScrollValue;
